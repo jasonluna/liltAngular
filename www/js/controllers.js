cf64816873a22c93c548f70de90476e775f9bf63
AliS86 <ali.sulta86@gmail.com>
2016-08-11 15:25:55 -0700

    commit message

---
+++ www/js/controllers.js
@@ -0,0 +1,119 @@
+angular.module('app.controllers', [])
+  
+.controller('lILTCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+ 
+
+app.controller('MainCtrl', ['$scope', '$cordovaCamera',
+
+function($scope, $cordovaCamera) {
+
+ $scope.takeImage = function() {
+
+    
+        var options = {
+            quality: 80,
+            destinationType: Camera.DestinationType.DATA_URL,
+            sourceType: Camera.PictureSourceType.CAMERA,
+            allowEdit: true,
+            encodingType: Camera.EncodingType.JPEG,
+            targetWidth: 250,
+            targetHeight: 250,
+            popoverOptions: CameraPopoverOptions,
+            saveToPhotoAlbum: false
+        };
+         
+        $cordovaCamera.getPicture(options).then(function(imageData) {
+            $scope.srcImage = "data:image/jpeg;base64," + imageData;
+        }, function(err) {
+            // error
+        });
+    }
+}])
+
+
+.controller('lILTStoryCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('cOMMUNITYCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('notificationsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('pROFILECtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('startPageCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+      
+.controller('registerCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('singleStoryCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('groupStoryCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('inviteCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('invite2Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+ 
---
+++ www/js/routes.js
@@ -0,0 +1,142 @@
+angular.module('app.routes', ['ionicUIRouter'])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      /* 
+    The IonicUIRouter.js UI-Router Modification is being used for this route.
+    To navigate to this route, do NOT use a URL. Instead use one of the following:
+      1) Using the ui-sref HTML attribute:
+        ui-sref='tabsController.lILT'
+      2) Using $state.go programatically:
+        $state.go('tabsController.lILT');
+    This allows your app to figure out which Tab to open this page in on the fly.
+    If you're setting a Tabs default page or modifying the .otherwise for your app and
+    must use a URL, use one of the following:
+      /page1/tab1/home
+      /page1/tab2/home
+  */
+  .state('tabsController.lILT', {
+    url: '/home',
+    views: {
+      'tab1': {
+        templateUrl: 'templates/lILT.html',
+        controller: 'lILTCtrl'
+      },
+      'tab2': {
+        templateUrl: 'templates/lILT.html',
+        controller: 'lILTCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.lILTStory', {
+    url: '/camera',
+    views: {
+      'tab2': {
+        templateUrl: 'templates/lILTStory.html',
+        controller: 'lILTStoryCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.cOMMUNITY', {
+    url: '/search',
+    views: {
+      'tab3': {
+        templateUrl: 'templates/cOMMUNITY.html',
+        controller: 'cOMMUNITYCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.notifications', {
+    url: '/notifications',
+    views: {
+      'tab5': {
+        templateUrl: 'templates/notifications.html',
+        controller: 'notificationsCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.pROFILE', {
+    url: '/profile',
+    views: {
+      'tab6': {
+        templateUrl: 'templates/pROFILE.html',
+        controller: 'pROFILECtrl'
+      }
+    }
+  })
+
+  .state('startPage', {
+    url: '/register',
+    templateUrl: 'templates/startPage.html',
+    controller: 'startPageCtrl'
+  })
+
+  .state('tabsController', {
+    url: '/page1',
+    templateUrl: 'templates/tabsController.html',
+    abstract:true
+  })
+
+  .state('register', {
+    url: '/page9',
+    templateUrl: 'templates/register.html',
+    controller: 'registerCtrl'
+  })
+
+  .state('tabsController.singleStory', {
+    url: '/single',
+    views: {
+      'tab2': {
+        templateUrl: 'templates/singleStory.html',
+        controller: 'singleStoryCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.groupStory', {
+    url: '/page15',
+    views: {
+      'tab2': {
+        templateUrl: 'templates/groupStory.html',
+        controller: 'groupStoryCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.invite', {
+    url: '/page13',
+    views: {
+      'tab2': {
+        templateUrl: 'templates/invite.html',
+        controller: 'inviteCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.invite2', {
+    url: '/page14',
+    views: {
+      'tab2': {
+        templateUrl: 'templates/invite2.html',
+        controller: 'invite2Ctrl'
+      }
+    }
+  })
+
+$urlRouterProvider.otherwise('/register')
+
+  
+
+});
